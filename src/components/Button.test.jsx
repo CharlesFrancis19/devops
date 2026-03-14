4689e974a19a280b6a5b534e27442ff9d8defbb6
CharlesFrancis19 <aakaashcharleswork@gmail.com>
2026-03-15 00:39:42 +1300

    added test workflow

---
+++ src/components/Button.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+test("renders button text", () => {
+  render(<Button />);
+  const buttonElement = screen.getByText("Click Me");
+  expect(buttonElement).toBeInTheDocument();
+});
